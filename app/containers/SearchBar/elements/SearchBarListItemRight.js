611be5ab2de6af7d9eb5e8bc8c60f048161a06fe
Kyle Henderson <kyle1101000@gmail.com>
2017-03-23 08:16:29 -0400

    Added arrow icons to the search results

--- app/containers/SearchBar/elements/SearchBarListItemRight.js
+++ app/containers/SearchBar/elements/SearchBarListItemRight.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import styled from 'styled-components';
 import { font, textSmall } from 'styles/text';
 import { red, lightBlue } from 'styles/colors';
+import SVG from 'components/SVG';
 
 const Wrapper = styled.span`
   margin-left: auto;
@@ -14,10 +15,22 @@ const Wrapper = styled.span`
 
 const ValueSpan = styled.span`
   color: ${(props) => (props.up ? lightBlue : red)};
+  display: flex;
+  flex-direction: row;
 `;
 
 const SearchResultStockRight = ({ value, prefix, up, ...props }) => (
-  <Wrapper {...props}>{prefix}<ValueSpan up={up}>{value}</ValueSpan></Wrapper>
+  <Wrapper {...props}>
+    {prefix}
+    <ValueSpan up={up}>
+      <SVG
+        style={{ margin: '0 5px 3px 0' }}
+        type={up ? 'arrow-up' : 'arrow-down'}
+        size="small"
+      />
+      {value}
+    </ValueSpan>
+  </Wrapper>
 );
 
 SearchResultStockRight.propTypes = {
